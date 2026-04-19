4a1e716a0ddd3ca834a768062e2d98a963cfb8d0
IjazNetworker <ijaj7330@gmail.com>
2026-04-19 17:45:36 +0530

    order model updated with address, quantity, status

--- orders/models.py
+++ orders/models.py
@@ -5,6 +5,9 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
+    address = models.TextField(default="Unknown")
+    quantity = models.IntegerField(default=1)
+    total_price = models.FloatField(default=10.0)
 
     status = models.CharField(
         max_length=20,
@@ -19,4 +22,4 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.customer_name} - {self.product.name}"
+        return self.customer_name
