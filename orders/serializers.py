178b1700fbe1d467d2ab3a9b5b44135a60a84e22
IjazNetworker <ijaj7330@gmail.com>
2026-04-21 22:33:56 +0530

    serializer for create order

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,21 +3,17 @@ from .models import Order
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = Product
         fields = ['id', 'name', 'image']
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if obj.image:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all()
+    )
+
+    product_details = ProductSerializer(source='product', read_only=True)
 
     class Meta:
         model = Order
